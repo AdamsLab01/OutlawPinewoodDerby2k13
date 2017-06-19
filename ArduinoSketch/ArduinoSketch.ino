9fd7a40abf5b477b65e0e88bea319ec89b1b47c7
rocketboy001 <adam@adambyers.com>
2017-06-19 07:55:12 -0700

    stuff happened

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -4,23 +4,6 @@ This sketch was written to control a motorized Pinewood Derby car via IR remote.
 
 For more information see - http://awaitinginspiration.com/2013/04/motorized-pinewood-derby-car-2013/ or ping adam@adambyers.com
 
-Coin Operated XBOX
-Copyright (C) 2013 Adam Byers
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 **********************************************************************************************************************/
 
 #include <IRremote.h> // Included the IR library
